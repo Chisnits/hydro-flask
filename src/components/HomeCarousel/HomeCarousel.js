a077b515b08c784ee67041c9d015178de9afc27d
Chase Derr <Chisnits@users.noreply.github.com>
2017-04-12 09:36:45 -0600

    Wednesday morning commit

--- src/components/HomeCarousel/HomeCarousel.js
+++ src/components/HomeCarousel/HomeCarousel.js
@@ -29,22 +29,24 @@ class HomeCarousel extends Component {
                 </div>
           </div>
             <div className="camping">
-                <div className="camping-text-slide-container">
-                    <span className="slide-two-line-one">
-                        Hydro Flask Presents
-                    </span>
-                    <span className="slide-two-line-two">
-                        constant motion
-                    </span>
-                    <span className="slide-two-line-three">
-                        Ride along with Quin Schrock as <br/>
-                        he explores the wilderness of <br/>
-                        Northern California, camera in hand. 
-                    </span>
-                    <span className="slide-two-line-4">
-                        watch the film
-                    </span>
-                </div>
+                <div className="camping-text">
+                    <div className="camping-text-slide-container">
+                        <span className="slide-two-line-one">
+                            Hydro Flask Presents
+                        </span>
+                        <span className="slide-two-line-two">
+                            constant motion
+                        </span>
+                        <span className="slide-two-line-three">
+                            Ride along with Quin Schrock as <br/>
+                            he explores the wilderness of <br/>
+                            Northern California, camera in hand. 
+                        </span>
+                        <span className="slide-two-line-4">
+                            watch the film
+                        </span>
+                    </div>
+                </div>    
             </div>
           <div className="sunset"></div>
         </Slider>
