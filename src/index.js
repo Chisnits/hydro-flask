fca23fac77f0e1a73b65a2b0445030813dd8cc20
Chase Derr <Chisnits@users.noreply.github.com>
2017-05-29 15:21:14 -0600

    set up routing in index.js using HashRouter

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+// import { Router, Route, hashHistory } from 'react-router';
+import { HashRouter as Router, Route } from 'react-router-dom';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render((
+  <Router>
+    <div>
+      <Route exact path = "/" component={ App }/>,
+    </div>
+  </Router>
+),
+  document.getElementById('root'))
