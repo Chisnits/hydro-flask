d0fa7aa3a90d06e1b58be6d2f3c7bc44a2ffcd29
Chase Derr <Chisnits@users.noreply.github.com>
2017-05-30 14:16:45 -0600

    set up server.js on port 3001

---
+++ db/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const port = 3001;
+
+let app = express();
+app.use(bodyParser.json());
+
+
+
+
+app.listen(port, function() {
+  console.log('Listening on ' + port);
+});
