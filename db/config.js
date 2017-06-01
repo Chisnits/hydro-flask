ae767f5824dd15e2dc226d46fb9e316a81366e72
Chase Derr <Chisnits@users.noreply.github.com>
2017-06-01 11:56:06 -0600

    working in server.js. Trying to get server to retrieve data from Elephant sql

---
+++ db/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    elephantSql:'postgres://ykawouko:AENISqpMGwJcdikmm4x9x6ZC4FZAWvOG@stampy.db.elephantsql.com:5432/ykawouko',
+    port:3001
+}
--- db/server.js
+++ db/server.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const port = 3001;
+const massive = require('massive');
+const cors = require('cors');
 
-let app = express();
+const app = module.exports = express();
+const config = require('./config.js');
+
 app.use(bodyParser.json());
+app.use(cors());
+
+// app.use(express.static('./src'));
 
 
-var gus = "EPIC"
+var db = massive.connectSync({
+  connectionString : config.elephantSql
+});
+  console.log(db);
 
-app.listen(port, function() {
-  console.log('Listening on ' + port);
+app.set('db', db);
+
+db.bottles_schema((err, data) => {
+  console.log(data)
+    if (err) console.log(err);
+    else console.log('All tables successfully! I mean Blammy!');
+})
+
+
+app.listen(config.port, function() {
+  console.log('Connected on port ' + config.port);
 });
