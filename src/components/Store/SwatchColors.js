2d4a4a01199d19d62e98fdd8bf1b2a800c313966
Chase Derr <Chisnits@users.noreply.github.com>
2017-06-07 11:04:48 -0600

    added swatch-colors into assets folder

---
+++ src/components/Store/SwatchColors.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import '../../styles/store/SwatchColors.css';
+
+class SwatchColors extends Component {
+
+    render() {
+       
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+
+export default SwatchColors;
