883b47d00a785d45077f24fefaf411bf46dcadf5
Chase Derr <Chisnits@users.noreply.github.com>
2017-06-08 12:17:24 -0600

    removed config.js

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,21 @@
+
+import React, { Component } from 'react';
+
+
+class Cart extends Component {
+ 
+
+
+
+    render() {
+        // console.log(this.props.bottles);
+        return (
+            <div id="cart" className="cart">
+
+            </div>
+        );
+    }
+}
+
+
+export default Cart;
